1e66f55585722da134402df5ac4eb11da08850c0
Hiren <hiren@Hirens-MacBook-Air.local>
2024-12-02 18:55:57 +0530

    add a .env file for port excess

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	app := fiber.New()
+	err := godotenv.Load()
+	if err!= nil {
+        fmt.Println("Error loading.env file")
+    }
+	port := os.Getenv("PORT");
+
+	if port == "" {
+        port = "3000"
+    }
+	fmt.Println(port)
+	app.Listen(":" + port)
 }
